ed8de599c8c70dfc92275c23ce014a32d8ba2308
fviktor92 <farkas.viktor92@gmail.com>
2020-09-19 14:35:35 +0200

    added miragejs

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import Particles from "react-particles-js";
+import { Server, Response } from "miragejs";
 
 const particlesOptions = {
     particles: {
@@ -31,3 +32,19 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// Delegate the requests to Mirage when Cypress loads the application
+if (window.Cypress) {
+    new Server({
+        environment: "test",
+        routes() {
+            let methods = ["get", "put", "patch", "post", "delete"]
+            methods.forEach((method) => {
+                this[method]("/*", async (schema, request) => {
+                    let [status, headers, body] = await window.handleFromCypress(request)
+                    return new Response(status, headers, body)
+                })
+            })
+        },
+    })
+}
